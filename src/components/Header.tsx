1146bd415195dce5a09685a4df7d63d4237d8bb2
taynguyen3110 <taynguyen3110@gmail.com>
2024-06-22 17:12:27 +1000

    complete: layout header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 import cartIcon from '../assets/icons/cart.png'
 import profileIcon from '../assets/icons/profile.png'
+import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { PromotionBar } from './PromotionBar'
 
 type Item = {
   id: number;
@@ -54,59 +57,81 @@ const handleOnFocus = () => {
 const formatResult = (item: Item) => {
   return (
     <>
-      <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
-      <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+      {/* <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span> */}
+      <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
     </>
   )
 }
 
 const Header = () => {
   return (
-    <div className='container mx-auto max-w-screen-2xl'>
-      <div className='promote-bar'>
-        <p className='text-center bg-black text-white text-xs/[38px]'>Sign up and get 20% off to your first order. <span className='underline font-medium'>Sign Up Now</span></p>
-      </div>
-      <div className='nav-bar flex gap-10 justify-center text-center mx-8 my-6'>
-        <div className='nav-logo relative'>
-          <p className='relative -top-1'>Shop.co</p>
-        </div>
-        <div className='nav-menu'>
-          <ul className='flex gap-6 h-full items-center'>
-            <li>Shop</li>
-            <li>On Sale</li>
-            <li>New Arrivals</li>
-            <li>Brands</li>
-          </ul>
-        </div>
-        <div className='nav-search'>
-          <div style={{ width: 400 }}>
-            <ReactSearchAutocomplete<Item>
-              items={items}
-              fuseOptions={{ keys: ["name", "description"] }}
-              resultStringKeyName="name"
-              inputDebounce={100}
-              onSearch={handleOnSearch}
-              onHover={handleOnHover}
-              onSelect={handleOnSelect}
-              onFocus={handleOnFocus}
-              maxResults={5}
-              placeholder="Search for products..."
-              autoFocus
-              styling={{
-                backgroundColor: '#F0EEED',
-                borderRadius: '20px',
-                boxShadow: 'none',
-                fontFamily: 'inherit',
-                fontSize: 'inherit',
-                color: 'rgba(0, 0, 0, 0.4)',
-              }}
-              formatResult={formatResult}
-            />
+    <div className='mx-auto'>
+      <PromotionBar />
+      <div className='container mx-auto nav-bar flex gap-10 justify-between text-center my-6'>
+        <div className='flex gap-x-10'>
+          <div className='nav-logo relative'>
+            <p className='relative -top-1'>Shop.co</p>
+          </div>
+          <div className='nav-menu flex gap-6 h-12 items-center'>
+            <div>
+              <Popover className="group">
+                <PopoverButton className="flex items-center gap-2">
+                  Shop
+                  <ChevronDownIcon className="size-5 group-data-[open]:rotate-180" />
+                </PopoverButton>
+                <Transition
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <PopoverPanel anchor="bottom" className="flex flex-col bg-background my-2 p-4 rounded">
+                    <a href="/insights">Zara</a>
+                    <a href="/automations">Calvine Klein</a>
+                    <a href="/reports">Prada</a>
+                  </PopoverPanel>
+                </Transition>
+              </Popover>
+            </div>
+            <div>On Sale</div>
+            <div>New Arrivals</div>
+            <div>Brands</div>
           </div>
         </div>
-        <div className='nav-btns flex items-center gap-3.5'>
-          <img src={cartIcon} alt="Shopping Cart" />
-          <img src={profileIcon} alt="Profile" />
+        <div className='flex gap-x-10'>
+          <div className='nav-search'>
+            <div style={{ width: 400 }}>
+              <ReactSearchAutocomplete<Item>
+                items={items}
+                fuseOptions={{ keys: ["name", "description"] }}
+                resultStringKeyName="name"
+                inputDebounce={100}
+                onSearch={handleOnSearch}
+                onHover={handleOnHover}
+                onSelect={handleOnSelect}
+                onFocus={handleOnFocus}
+                maxResults={5}
+                placeholder="Search for products..."
+                autoFocus
+                showIcon={false}
+                styling={{
+                  backgroundColor: '#F0EEED',
+                  borderRadius: '20px',
+                  boxShadow: 'none',
+                  fontFamily: 'inherit',
+                  fontSize: 'inherit',
+                  color: 'rgba(0, 0, 0, 0.4)',
+                }}
+                formatResult={formatResult}
+              />
+            </div>
+          </div>
+          <div className='nav-btns flex items-center gap-3.5'>
+            <img src={cartIcon} alt="Shopping Cart" />
+            <img src={profileIcon} alt="Profile" />
+          </div>
         </div>
       </div>
     </div>
