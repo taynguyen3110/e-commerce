dce55aab6b60836d1af12f62727f3590af2b18e6
taynguyen3110 <taynguyen3110@gmail.com>
2024-06-26 15:05:17 +1000

    styling: homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -6,23 +6,23 @@ const Homepage = () => {
   return (
     <div className='h-[663px] bg-[#F2F0F1]'>
       <div className='container flex justify-between h-full gap-12'>
-        <div className='w-1/2 flex-col'>
+        <div className='w-1/2 flex flex-col gap-8 items-start justify-center'>
           <h1 className='text-6xl'>FIND CLOTHES THAT MATCH YOUR STYLE</h1>
           <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style</p>
-          <button className=''>Shop Now</button>
-          <div className='flex'>
+          <button className='px-16 py-[15px] bg-black text-white rounded-[27px]'>Shop Now</button>
+          <div className='flex justify-between w-full'>
             <div>
-              <p>200+</p>
+              <p className='text-[40px] font-bold'>200+</p>
               <p>International Brands</p>
             </div>
-            <div>l</div>
+            <div className='border-l-2 border-black opacity-10'></div>
             <div>
-              <p>2,000+</p>
+              <p className='text-[40px] font-bold'>2,000+</p>
               <p>High-Quality Products</p>
             </div>
-            <div>l</div>
+            <div className='border-l-2 border-black opacity-10'></div>
             <div>
-              <p>30,000+</p>
+              <p className='text-[40px] font-bold'>30,000+</p>
               <p>Happy Customers</p>
             </div>
           </div>
